c2dc7b4f96ef1a2afe2c6d3d1db741ae05a0b756
starcoex <starcoex@naver.com>
2024-07-20 00:14:08 +0900

    gatsby totorial part2

--- servers/apps/gateway/src/app.module.ts
+++ servers/apps/gateway/src/app.module.ts
@@ -5,36 +5,65 @@ import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
 import { authContext } from './auth.context';
 import { LoggerModule } from '@app/common/logger';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+    // GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+    //   driver: ApolloGatewayDriver,
+    //   server: {
+    //     context: authContext,
+    //   },
+    //   gateway: {
+    //     supergraphSdl: new IntrospectAndCompose({
+    //       subgraphs: [
+    //         {
+    //           name: 'users',
+    //           url: 'http://localhost:3001/graphql',
+    //         },
+    //       ],
+    //     }),
+    //     buildService({ url }) {
+    //       return new RemoteGraphQLDataSource({
+    //         url,
+    //         willSendRequest({ request, context }) {
+    //           request.http.headers.set(
+    //             'user',
+    //             context.user ? JSON.stringify(context.user) : null,
+    //           );
+    //         },
+    //       });
+    //     },
+    //   },
+    // }),
+    GraphQLModule.forRootAsync<ApolloGatewayDriverConfig>({
       driver: ApolloGatewayDriver,
-      server: {
-        context: authContext,
-      },
-      gateway: {
-        supergraphSdl: new IntrospectAndCompose({
-          subgraphs: [
-            {
-              name: 'users',
-              url: 'http://localhost:3001/graphql',
-            },
-          ],
-        }),
-        buildService({ url }) {
-          return new RemoteGraphQLDataSource({
-            url,
-            willSendRequest({ request, context }) {
-              request.http.headers.set(
-                'user',
-                context.user ? JSON.stringify(context.user) : null,
-              );
-            },
-          });
+      useFactory: (configService: ConfigService) => ({
+        gateway: {
+          supergraphSdl: new IntrospectAndCompose({
+            subgraphs: [
+              {
+                name: 'users',
+                url: configService.getOrThrow('USERS_GRAPHQL_RUL'),
+              },
+            ],
+          }),
+          buildService({ url }) {
+            return new RemoteGraphQLDataSource({
+              url,
+              willSendRequest({ request, context }) {
+                request.http.headers.set(
+                  'user',
+                  context.user ? JSON.stringify(context.user) : null,
+                );
+              },
+            });
+          },
         },
-      },
+      }),
+      inject: [ConfigService],
     }),
+    ConfigModule.forRoot({ isGlobal: true }),
     LoggerModule,
   ],
   controllers: [],
